9e3c2ba5fe47ba336c8f2766e0455407e5a9b7a3
leehjung55 <leehjung55@gmail.com>
2021-03-29 22:10:13 +0900

    MEMBER

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -1,7 +1,11 @@
 package hello.core.discount;
 
+import hello.core.member.Member;
+
 public interface DiscountPolicy {
     /**
      * @return 할인 대상 금액
      */
+    // 호출하고 나면, 얼마가 할인 됬는지 return 해줌.
+    int discount(Member member, int price);
 }
--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,7 +1,23 @@
 package hello.core.discount;
 
-public class FixDiscountPolicy {
+import hello.core.member.Grade;
+import hello.core.member.Member;
 
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    // 우린 무조건 1000원 할인해주니깐, 이런식으로 작성!
     private int discountFixAmount = 1000; // 1000 원할인
 
+    @Override
+    public int discount(Member member, int price) {
+
+        // 조건. 등급이 vip이면, 위의 해당.
+        if (member.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        }
+        else{
+            return 0;
+        }
+
+    }
 }
