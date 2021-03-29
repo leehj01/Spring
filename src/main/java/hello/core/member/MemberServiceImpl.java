87f05e32f97df038e80b9f1775124995c50a589f
leehjung55 <leehjung55@gmail.com>
2021-03-29 22:46:42 +0900

    MEMBER

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,9 +5,11 @@ public class MemberServiceImpl implements MemberService{
     // 가입을 하고 회원을 찾으려면, 앞에서 만든 MemberRepository 가 필요함
     // 인터페이스를 적었으나, 구현체 없이 인터페이스만 가지면 null point 가 됨
     // 따라서 구현체(MemoryMemberRepository) 를 선택해줘야함.
+
+    // 이건 왼쪽(추상화)에도 의존하고 오른쪽(할당하는 부분)또한 구현체를 의존함 -> 변경이 있을 때, 문제가 됨 = dip를 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    // 조인에서 save를 호출하면, 다형성에 따라서 MemoryMemberRepository 가 호출됨
+    // 조인에서 save를 호출하면, 다형성에 따라서 MemoryMemberRepository 가호출됨
     @Override
     public void join(Member member) {
         memberRepository.save(member);
