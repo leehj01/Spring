d5475174229868c160d14bdd51bdbad86673bb65
leehjung55 <leehjung55@gmail.com>
2021-03-29 20:38:08 +0900

    Initial commit

---
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,7 @@
+package hello.core.discount;
+
+public interface DiscountPolicy {
+    /**
+     * @return 할인 대상 금액
+     */
+}
---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,7 @@
+package hello.core.discount;
+
+public class FixDiscountPolicy {
+
+    private int discountFixAmount = 1000; // 1000 원할인
+
+}
