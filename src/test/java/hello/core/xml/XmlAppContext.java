308ddc8cc0f2d7da7953acdd3b952fb8b3590c5c
leehjung55 <leehjung55@gmail.com>
2021-03-31 08:41:40 +0900

    beandefinition

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+// xml 기반으로 설정 사용
+public class XmlAppContext {
+
+    // config.xml은 위의 resources에 두기 ( 자바코드가 아닌 것은 다 여기에 ! )
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+
+}
