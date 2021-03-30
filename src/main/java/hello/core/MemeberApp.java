1dafc97272b95cf5078c3a90719c03319b47b51b
leehjung55 <leehjung55@gmail.com>
2021-03-30 15:45:30 +0900

    MEMBER

--- src/main/java/hello/core/MemeberApp.java
+++ src/main/java/hello/core/MemeberApp.java
@@ -1,11 +1,23 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemeberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        // config 전
+        // MemberService memberService = new MemberServiceImpl();
+
+        // spring 쓰기 전 ,
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
+         Member member = new Member(1L, "memberA", Grade.VIP);
         //회원가입
         memberService.join(member);
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,20 +7,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // order 가 잘되는지 test하는 코드
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        // config 전
+        // MemberService memberService = new MemberServiceImpl();
+        // OrderService orderService = new OrderServiceImpl();
 
-        // 멤버 아이디를 먼저 저장하기 위해서, 아래의 코드를 적음
-        long memeberId = 1L;
-        // vip를 만듦
-        Member member = new Member(memeberId, "memberA", Grade.VIP);
-        // 먼저,메모리에 정보를 넣음
-        memberService.join(member);
+        // spring 전
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        long memeberId = 1L; // 멤버 아이디를 먼저 저장하기 위해서
+        Member member = new Member(memeberId, "memberA", Grade.VIP); // vip를 만듦
+        memberService.join(member);  // 먼저,메모리에 정보를 넣음
 
         // 주문을 하는 코드
         Order order = orderService.createOrder(memeberId, "itemA", 10000);
