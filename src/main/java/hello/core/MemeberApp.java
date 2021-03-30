a64c2d50f0267bca47ce7f19c1183a7eb20666bf
leehjung55 <leehjung55@gmail.com>
2021-03-31 03:17:19 +0900

    MEMBER

--- src/main/java/hello/core/MemeberApp.java
+++ src/main/java/hello/core/MemeberApp.java
@@ -13,7 +13,12 @@ public class MemeberApp {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 
+        // ApplicationContext : 스프링 컨테이너 - 다 담아주는 아이
+        // appconfig에 있는 환경설정 가지고, @bean 붙어있는것들을 관리해줌,
+        // 직접 찾아오는 것이 아니라, 스프링 컨테이너에 담아서 가져오게 되는것
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 이름은 이거고, 뒤에는 타입을 불러온다는 의미.
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
 
