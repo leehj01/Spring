583fd5e441272c390b72b0155b1081a53ebc4243
leehjung55 <leehjung55@gmail.com>
2021-04-01 23:58:15 +0900

    연관관계 주입

---
+++ src/test/java/hello/core/order/order/OrderServiceTest.java
@@ -0,0 +1,40 @@
+package hello.core.order.order;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    // MemberService memberService = new MemberServiceImpl();
+    // OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+    @Test
+    void createOrder(){
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        // 이걸 써야, 편하게 test를 만들수 있음
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+
+}
